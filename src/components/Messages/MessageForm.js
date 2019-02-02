f6cb01e0c1c7cf285e6092905d13bf788ba0e224
peter-siebold <peter.siebold@icloud.com>
2019-02-02 06:20:06 +0100

    cancel upload tasks on unmount

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -24,6 +24,15 @@ class MessageForm extends React.Component{
         user: this.props.currentUser,
     }
     
+    componentWillUnmount() {
+        if(this.state.uploadTask !== null) {
+            this.state.uploadTask.cancel();
+            this.setState({
+                uploadTask: null
+            })
+        }
+    }
+
     openModal = () => this.setState({ modal: true})
     closeModal = () => this.setState({ modal : false})
 
