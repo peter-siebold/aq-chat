0e80e989dd226f9b935d131f985d1a7955ed5d4d
peter-siebold <peter.siebold@icloud.com>
2019-02-02 06:38:38 +0100

    remove channel listeners on unmount

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -130,6 +130,9 @@ class Channels extends React.Component{
     }
     removeListeners = () => {
         this.state.channelsRef.off();
+        this.state.channels.forEach(channel => {
+            this.state.messagesRef(channel.id).off();
+        })
     }
 
     setFirstChannel = () => {
